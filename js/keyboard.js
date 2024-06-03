6560ff2e9eef07234f5e1225041476d5d29b809a
MagicJoe <mag1ctr1cks@hotmail.com>
2024-06-03 22:31:19 +0700

    adding the keyboard for the main game by pushing it

---
+++ js/keyboard.js
@@ -0,0 +1,37 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const input = document.getElementById('word-input');
+    input.focus();
+    window.addEventListener(
+        "keydown",
+        (event) => {
+            event.preventDefault();
+            if (event.keyCode >= 65 && event.keyCode <= 90 && input.textContent.length < 5){
+                event.preventDefault();
+                input.textContent += event.key;
+            } else if (event.keyCode == 8){
+                event.preventDefault();
+                input.textContent = input.textContent.length == 0 ? '' : input.textContent.slice(0, -1);
+            }
+        },
+        true,
+    );
+    const keyboard = document.getElementById('keyboard');
+    let keyRows = keyboard.childNodes;
+    for(const row of keyRows){
+        let keys = row.childNodes;
+        for(const key of keys){
+            if(key.classList == 'key'){
+                key.addEventListener('click', (event) => {
+                    if(input.textContent.length < 5){
+                        input.textContent += key.textContent;
+                    }            
+                }, true,)
+            }
+        }
+    }
+    const back = document.getElementById('BACK');
+    back.addEventListener('click', (event) => {
+        input.textContent = input.textContent.length == 0 ? '' : input.textContent.slice(0, -1);
+    }, true,);
+});
