29179350612817be741bf96b64d959c24897d4d7
MagicJoe <mag1ctr1cks@hotmail.com>
2024-06-16 12:02:58 +0700

    encoding params

--- js/decipher.js
+++ js/decipher.js
@@ -1,6 +1,11 @@
-const urlParams = new URLSearchParams(window.location.search);
-const score = urlParams.get('score');
-const time = urlParams.get('time');
+let urlParams = new URLSearchParams(window.location.search);
+const code = urlParams.get('code');
+if(code){
+    const decrypted = hexToString(code);
+    urlParams = new URLSearchParams(decrypted);
+}
+let score = urlParams.get('score');
+let time = urlParams.get('time');
 let theirWords = urlParams.get('words');
 theirWords = theirWords.split(':').map(w => {return words[w]})
 let choWords = Cookies.get('chosen-words');
@@ -38,3 +43,13 @@ document.addEventListener('DOMContentLoaded', () => {
         yourWords.appendChild(preWordDiv);
     });
 });
+
+function hexToString(hex) {
+    let str = '';
+    for (let i = 0; i < hex.length; i += 2) {
+        let hexCode = hex.substr(i, 2);
+        let charCode = parseInt(hexCode, 16);
+        str += String.fromCharCode(charCode);
+    }
+    return str;
+}
