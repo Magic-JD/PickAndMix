a9467b176d20d85b4b22a6643e47e2b1a914dd63
MagicJoe <mag1ctr1cks@hotmail.com>
2024-09-07 20:29:15 +0700

    Fixing date so that it will calculate the correct number of days

--- src/utils/TimeUtils.js
+++ src/utils/TimeUtils.js
@@ -1,5 +1,6 @@
 export function getYourPuzzleId() {
     const startDate = new Date('2024-09-04');
+    startDate.setHours(0, 0, 0, 0);
     const currentDate = new Date();
     const differenceInMillis = currentDate - startDate;
     const differenceInDays = Math.floor(differenceInMillis / (1000 * 60 * 60 * 24));
