cbfe0d0749a418c5a2b25511e72adccd98b5ed91
MagicJoe <mag1ctr1cks@hotmail.co.uk>
2024-12-13 09:16:06 +0700

    Adding words for the 14th and 15th day

--- src/data/words.js
+++ src/data/words.js
@@ -1551,8 +1551,8 @@ export function getTodaysWords(lang){
 }
 
 
-const adventWordStart = ["SLICK", "PUNCH", "BIRDS", "JUNKY", "BIGHT", "DITZY", "FUNGI", "KRONE", "HONED", "DERBY", "WORKS", "DOUBT", "GRIPY"];
-const adventWordEnd = ["NYMPH", "BEFIT", "WHOMP", "SCOWL", "CLUNK", "HULKS", "JAPED", "WHUMP", "BUMPY", "FLUNK", "LIMBY", "FAWNY", "LUNCH"];
+const adventWordStart = ["SLICK", "PUNCH", "BIRDS", "JUNKY", "BIGHT", "DITZY", "FUNGI", "KRONE", "HONED", "DERBY", "WORKS", "DOUBT", "GRIPY", "HAVOC", "GLOWS"];
+const adventWordEnd = ["NYMPH", "BEFIT", "WHOMP", "SCOWL", "CLUNK", "HULKS", "JAPED", "WHUMP", "BUMPY", "FLUNK", "LIMBY", "FAWNY", "LUNCH", "QUITE", "MIRKY"];
 
 export function getAdventWordStart(number){
     return adventWordStart[number - 1];
