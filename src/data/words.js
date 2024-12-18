f430a6c02c7d25d0e353e518e88c417a0eaca0f2
MagicJoe <mag1ctr1cks@hotmail.co.uk>
2024-12-18 22:13:41 +0700

    adding for the 16th 17th

--- src/data/words.js
+++ src/data/words.js
@@ -1551,8 +1551,8 @@ export function getTodaysWords(lang){
 }
 
 
-const adventWordStart = ["SLICK", "PUNCH", "BIRDS", "JUNKY", "BIGHT", "DITZY", "FUNGI", "KRONE", "HONED", "DERBY", "WORKS", "DOUBT", "GRIPY", "HAVOC", "GLOWS"];
-const adventWordEnd = ["NYMPH", "BEFIT", "WHOMP", "SCOWL", "CLUNK", "HULKS", "JAPED", "WHUMP", "BUMPY", "FLUNK", "LIMBY", "FAWNY", "LUNCH", "QUITE", "MIRKY"];
+const adventWordStart = ["SLICK", "PUNCH", "BIRDS", "JUNKY", "BIGHT", "DITZY", "FUNGI", "KRONE", "HONED", "DERBY", "WORKS", "DOUBT", "GRIPY", "HAVOC", "GLOWS", "FAGIN", "HYPED"];
+const adventWordEnd = ["NYMPH", "BEFIT", "WHOMP", "SCOWL", "CLUNK", "HULKS", "JAPED", "WHUMP", "BUMPY", "FLUNK", "LIMBY", "FAWNY", "LUNCH", "QUITE", "MIRKY", "HULKY", "FLOCK"];
 
 export function getAdventWordStart(number){
     return adventWordStart[number - 1];
